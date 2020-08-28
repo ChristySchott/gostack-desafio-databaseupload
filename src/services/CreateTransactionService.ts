755320ef5d54a87f98ce84ed7a72ddb1cb2e6f41
christy <christy@christy.meuintelbras.local>
2020-08-28 14:03:29 -0300

    feat: check if category already exists

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,7 @@
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import AppError from '../errors/AppError';
 
@@ -14,13 +15,34 @@ interface Request {
 class CreateTransactionService {
   public async execute({ title, value, type, category_id }: Request): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
-    const balance = await transactionsRepository.getBalance()
+
+    const categoriesRepository = getRepository(Category);
+
+    const categoryAlreadyExists = await categoriesRepository.findOne({
+      where: {
+        id: category_id
+      }
+    })
+
+    if (categoryAlreadyExists) {
+
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    const transactionCategoryAlreadyExists = await transactionsRepository.findOne({
+      where: category_id,
+    });
+
+    if (transactionCategoryAlreadyExists) {
+
+    }
 
     if (type !== 'income' && type !== 'outcome') {
       throw new AppError('Type should be income or outcome.');
     }
 
-    if (type === 'outcome' && value > balance.income) {
+    if (type === 'outcome' && value > total) {
       throw new AppError('Insufficient funds.');
     }
 
