43f05b7851f703a44f12585abd0b6576771973e3
christy <christy@christy.meuintelbras.local>
2020-08-28 13:17:24 -0300

    feat: create delete services

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, DeleteResult } from 'typeorm';
+
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<DeleteResult> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository)
+
+    const deleteTransaction = await transactionsRepository.delete(id)
+
+    if (!(await deleteTransaction).affected) {
+      throw new AppError('Transaction not deleted, ID not found.');
+    }
+
+    return deleteTransaction;
   }
 }
 
