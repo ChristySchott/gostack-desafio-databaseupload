fcbe66466624643f66a2f4991a79980a2ba89142
christy <christy@christy.meuintelbras.local>
2020-08-28 12:32:40 -0300

    feat: call category router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import transactionsRouter from './transactions.routes';
+import categorysRouter from './categorys.routes';
 
 const routes = Router();
 
+routes.use('/categorys', categorysRouter);
 routes.use('/transactions', transactionsRouter);
 
 export default routes;
